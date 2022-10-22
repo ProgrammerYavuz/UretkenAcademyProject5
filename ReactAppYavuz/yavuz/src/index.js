0a342230bbe03e65e0442ff7b5f07784f09dc83c
ProgrammerYavuz <109480983+ProgrammerYavuz@users.noreply.github.com>
2022-10-22 23:47:39 +0300

    Update index.js

--- ReactAppYavuz/yavuz/src/index.js
+++ ReactAppYavuz/yavuz/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-//let deger = document.querySelector('.deger');
-//console.log(deger)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
